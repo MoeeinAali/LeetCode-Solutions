4b2a0b85c8920db49731c53b5146f4a6ff43c252
Moeein Aali <moeeeinaali@gmail.com>
2024-06-21 12:17:59 +0330

    Solve Array Reduce Transformation

---
+++ Study-Plans/30 Days of JavaScript/Basic Array Transformations/Array Reduce Transformation.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+    if (nums.length == 0) {
+        return init;
+    }
+    var result = fn(init, nums[0]);
+    for (var i = 1; i < nums.length; i++) {
+        result = fn(result, nums[i]);
+    }
+    return result;
+
+};
