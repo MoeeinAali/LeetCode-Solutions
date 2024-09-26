c6aa0e76a36b189095959e63c1b1ce90d925c88b
Moeein Aali <moeeeinaali@gmail.com>
2024-09-26 19:16:54 +0330

    Memorize.js

--- Study-Plans/30 Days of JavaScript/Function Transformations/Memoize.js
+++ Study-Plans/30 Days of JavaScript/Function Transformations/Memoize.js
@@ -3,20 +3,24 @@
  * @return {Function}
  */
 function memoize(fn) {
-
+    const cache = {}
     return function (...args) {
-
+        const key = JSON.stringify(args)
+        if (cache[key] !== undefined) {
+            return cache[key]
+        }
+        const result = fn(...args)
+        cache[key] = result
+        return result
     }
 }
 
 
-/** 
- * let callCount = 0;
- * const memoizedFn = memoize(function (a, b) {
- *	 callCount += 1;
- *   return a + b;
- * })
- * memoizedFn(2, 3) // 5
- * memoizedFn(2, 3) // 5
- * console.log(callCount) // 1 
- */
+// let callCount = 0;
+// const memoizedFn = memoize(function (a, b) {
+//     callCount += 1;
+//     return a + b;
+// })
+// console.log(memoizedFn(2, 3)) // 5
+// console.log(memoizedFn(2, 3)) // 5
+// console.log(callCount) // 1
