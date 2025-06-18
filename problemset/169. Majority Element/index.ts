c3846d9f880c7166314236b4dd6d7f008abe3fc9
Moeein Aali <moeeeinaali@gmail.com>
2025-06-18 13:23:38 +0330

    [ADD] 169

---
+++ problemset/169. Majority Element/index.ts
@@ -0,0 +1,18 @@
+function majorityElement(nums: number[]): number {
+    let i = 1;
+    let element = nums[0];
+    let count = 1;
+    while (i < nums.length) {
+        if (nums[i] !== element) {
+            count--;
+        } else {
+            count++;
+        }
+        if (count < 0) {
+            element = nums[i];
+            count = 0;
+        }
+        i++;
+    }
+    return element;
+}
